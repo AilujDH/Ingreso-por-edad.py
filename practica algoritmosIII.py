b7860660953b8fd481e523b7dd10a3be1dddd0a6
AilujDH <ailujfdz@gmail.com>
2024-10-04 11:47:44 +0200

    Realizamos un programa que indica si podemos ingresar o no a la discoteca segun la edad

---
+++ practica algoritmosIII.py
@@ -0,0 +1,13 @@
+
+edad= int (input( "Por favor ingrese su edad: "))
+
+permitido= True if edad>=18 else False
+
+if permitido:
+    print ("Puede entrar a la discoteca")
+else:
+    print ("Lo sentimos, no puede ingresar a la discoteca")
+    
+
+
+
